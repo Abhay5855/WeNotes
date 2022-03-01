edec764bc9810057af922250d52dd460d325717a
Abhay5855 <ap3919529@gmail.com>
2022-03-01 16:18:28 +0530

    style : adding styles to signup buuton and navbar items

--- src/components/landing/LandingNavbar.js
+++ src/components/landing/LandingNavbar.js
@@ -1,29 +1,28 @@
-
-
-
-import React from 'react'
-import './landingpage.css'
+import React from "react";
+import "./landingpage.css";
 
 const LandingNavbar = () => {
   return (
-    <nav className='landing__navbar'>
+    <nav className="landing__navbar">
+      <ul>
+        <li>
+          <div className="landing__navbar__image"></div>
+        </li>
 
+        <li>
+          <h3><span className="heading">WeNotes</span>, Write better, faster, smarter</h3>
+        </li>
 
-        <ul>
-            <li>
-                <div className='landing__navbar__image'>
+        <li>
+          <p className="login">Login</p>
+        </li>
 
-                </div>
-            </li>
-
-            <li>
-                
-            </li>
-        </ul>
-
-          
+        <li>
+          <button className="landing__navbar__btn">Sign up</button>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default LandingNavbar
+export default LandingNavbar;
