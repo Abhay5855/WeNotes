e5c8cf7e62dd13cf6d3bed6649c85425ddda38aa
Abhay5855 <ap3919529@gmail.com>
2022-03-01 19:07:17 +0530

    docs : adding the components into seperate route using React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
 import LandingPage from './components/landing/LandingPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />}></Route>
+      </Routes>
+      </BrowserRouter>
 
-        <LandingPage />
+      
    
     </div>
   );
