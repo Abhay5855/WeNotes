1d776b61961a3e906be88fac79703f7c1831f5d5
Abhay5855 <ap3919529@gmail.com>
2022-03-01 21:40:52 +0530

    docs: adddin the fonts and elements of the sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,12 +5,26 @@ const Sidebar = () => {
   return (
     <div className="sidebar__container">
       <div className="sidebar__elements">
-        <h2>Heena</h2>
-        <h2>Heena</h2>
-        <h2>Heena</h2>
-        <h2>Heena</h2>
-        <h2>Heena</h2>
-        <p>heenasnsdkdkdmkdm</p>
+        <div>
+          <i class="far fa-sticky-note"></i>
+
+          <h3>Notes</h3>
+        </div>
+
+        <div>
+          <i class="far fa-star"></i>
+          <h3>Favorites</h3>
+        </div>
+
+        <div>
+          <i class="fas fa-hashtag"></i>
+          <h3>Tags</h3>
+        </div>
+
+        <div>
+        <i class="fas fa-th-list"></i>
+         <h3>Todos</h3>
+        </div>
       </div>
     </div>
   );
