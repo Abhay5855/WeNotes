0241a4f48a57537e7078c2460ae919a29592dce6
Abhay5855 <ap3919529@gmail.com>
2022-03-02 15:01:10 +0530

    fix : remove the flex property from the chid div the route elements were stacking on top of each other

--- src/notes/Notes.js
+++ src/notes/Notes.js
@@ -1,12 +1,31 @@
-
-
-
-import React from 'react'
+import React from "react";
+import './notes.css'
 
 const Notes = () => {
   return (
-    <div>Notes</div>
-  )
-}
+    <div className="notes__container">
+      <header className="notes__header">
+        <ul>
+          <li>
+            <h3 style={{color : '#fff'}} className="notes__heading">Notes</h3>
+          </li>
 
-export default Notes
+          <li>
+            <div>
+              <i class="fas fa-search"></i>
+            </div>
+          </li>
+
+          <li>
+            <div>
+            <i class="fas fa-plus-circle"></i>
+            </div>
+          </li>
+        </ul>
+      </header>
+    </div>
+  
+  );
+};
+
+export default Notes;
