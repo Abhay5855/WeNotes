4710983bc576f1277d4f09f8cd41b23c467a244e
Abhay5855 <ap3919529@gmail.com>
2022-03-05 00:46:54 +0530

    docs : using the useEffect calling the notes added in firebase and srtting the note state with the data we get.

--- src/components/main_components/Main.js
+++ src/components/main_components/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../sidebar/Sidebar";
 import Editor from "../editor/Editor";
 import Notes from "../../notes/Notes";
@@ -6,41 +6,52 @@ import Tasks from "../tasks/Tasks";
 import "../sidebar/sidebar.css";
 import Tags from "../tags/Tags";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { db } from "../../firebase/firebase";
+import { query, onSnapshot , collection} from "firebase/firestore";
 
-
-import './main.css'
-
+import "./main.css";
 
 const Main = () => {
+  const [notes, setNotes] = useState([]);
+
+  // get all the notes added iside the effect
+  useEffect(() => {
+    const GetAllNotes = () => {
+      const allNotes = query(collection(db, "note"));
+
+      onSnapshot(allNotes, (QuerySnapshot) => {
+        setNotes(
+          QuerySnapshot.docs.map((doc) => ({
+            data: doc.data(),
+          }))
+        );
+      });
+    };
+
+    GetAllNotes();
+  }, []);
+
+  console.log(notes);
   return (
     <div className="main__container">
-
       <div className="main__items">
-
-        
-
-      <Router>
-        <Sidebar />
-        <Switch>
-          <Route path="/notes">
-            <Notes />
-          </Route>
-          <Route path="/tasks">
-            <Tasks />
-          </Route>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-
-         
-        </Switch>
-        
-      </Router>
-
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route path="/notes">
+              <Notes />
+            </Route>
+            <Route path="/tasks">
+              <Tasks />
+            </Route>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+          </Switch>
+        </Router>
       </div>
 
-      <Editor  />
-  
+      <Editor />
     </div>
   );
 };
