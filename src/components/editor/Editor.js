e9fc8b6913fd62f55555e150e14d299070a6daf8
Abhay5855 <ap3919529@gmail.com>
2022-03-04 20:40:27 +0530

    docs : create a state for the qull editor body and we are setting the state in handleChange function.

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./editor.css";
 import ReactQuill from "react-quill";
 import Editor_Format, { modules, formats } from "../../utils/Editor_Format";
 
+
 const Editor = () => {
- 
+
+
+
+  //state to add text inside of editor
+  const [text , setText] = useState('');
+
+
+
+  const handleChange = (val) => {
+
+       
+          setText(val);
+
+          console.log(text);
+  }
+
+    
   return (
     <div className="editor__container">
       <div className="editor">
         <Editor_Format />
-        <ReactQuill
-          modules={modules}
-          formats={formats}
-        />
+        <ReactQuill modules={modules} formats={formats} value={text} onChange={handleChange}/>
       </div>
     </div>
   );
