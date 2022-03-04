1ec90b544e993b741048f292038c488ab692e781
Abhay5855 <ap3919529@gmail.com>
2022-03-04 14:40:47 +0530

    docs : add the util editor to the editor and pass the styles from the make styles from material ui

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,17 +1,54 @@
+import React from "react";
+import "./editor.css";
+import ReactQuill from "react-quill";
+import { makeStyles } from "@material-ui/core";
+import Editor_Format, { modules, formats } from "../../utils/Editor_Format";
+import "react-quill/dist/quill.snow.css";
 
-
-
-import React from 'react'
-import './editor.css'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+  },
+  titleInputWrapper: {
+    marginBottom: "1rem",
+  },
+  titleInput: {
+    outline: "none",
+    width: "100%",
+    padding: "0.8rem 1rem",
+    marginTop: "1rem",
+    fontSize: "1rem",
+    fontFamily: "inherit",
+  },
+  editor: {
+    "& .ql-container.ql-snow": {
+      fontSize: theme.spacing(1),
+      fontFamily: "inherit",
+      wordBreak: "break-all",
+      minHeight: "20vh",
+      [theme.breakpoints.up("sm")]: {
+        minHeight: "calc(100vh - 15rem)",
+      },
+    },
+  },
+}));
 
 const Editor = () => {
+  const classes = useStyles();
   return (
-    <div className='editor__container'>
-         <div className='editor'>
-           
-         </div>
+    <div className="editor__container">
+      <div className="editor">
+        <Editor_Format />
+        <ReactQuill
+          className={classes.editor}
+          modules={modules}
+          formats={formats}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
