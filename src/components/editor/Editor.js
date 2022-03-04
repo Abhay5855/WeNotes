701fa01ebfacf196a2559f3667706460aa6b0bf7
Abhay5855 <ap3919529@gmail.com>
2022-03-05 00:32:47 +0530

    docs : adding the title and the text i.e the body to the firebase databse.

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -3,14 +3,16 @@ import "./editor.css";
 import ReactQuill from "react-quill";
 import Editor_Format, { modules, formats } from "../../utils/Editor_Format";
 import { debounce } from "lodash";
+import { db } from "../../firebase/firebase";
+import { query, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const Editor = () => {
   //state to add text inside of editor
   const [text, setText] = useState("");
   const [id, setId] = useState("");
   const [title, setTitle] = useState("");
-  const [addingNote , setAddingNote] = useState(false);
-
+  const [addingNote, setAddingNote] = useState(false);
 
   //using the useCallback hook to pass the debounce val and time delay of 1sec.
   const deb = useCallback(
@@ -25,14 +27,26 @@ const Editor = () => {
   };
 
   // handle submit button
-  const handleAdd = () => {
+  const handleAdd = async () => {
+    setAddingNote(true);
+    console.log("added");
 
-         setAddingNote(true);
-         console.log('added');
-  }
+    await addDoc(collection(db, "note"), {
+      title: title,
+      text: text,
+      timestamp: serverTimestamp(),
+    })
+      .then((res) => {
+        console.log(res);
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // Disable condition if text is empty
-  const disabled =  !title;
+  const disabled = !title;
 
   return (
     <div className="editor__container">
@@ -40,11 +54,20 @@ const Editor = () => {
         {/* Using dangerousHtml property to remove the html tags and display plain text */}
         {/* <h2 dangerouslySetInnerHTML={{ __html: text }} /> */}
 
-        <button disabled={disabled} className="submit__note"  onClick={handleAdd}>
-            Submit Note
+        <button
+          disabled={disabled}
+          className="submit__note"
+          onClick={handleAdd}
+        >
+          Submit Note
         </button>
-        <input type='text' placeholder='Enter your note title..' value={title} onChange={(e) => setTitle(e.target.value)}/>
-        
+        <input
+          type="text"
+          placeholder="Enter your note title.."
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
         <Editor_Format />
         <ReactQuill
           modules={modules}
--- src/notes/Notes.js
+++ src/notes/Notes.js
@@ -3,30 +3,20 @@ import "./notes.css";
 
 const Notes = () => {
   return (
-  
-      <div className="notes__header">
-        <ul>
-          <li>
-            <h3  className="notes__heading">
-              Notes
-            </h3>
-          </li>
+    <div className="notes__header">
+      <ul>
+        <li>
+          <h3 className="notes__heading">Notes</h3>
+        </li>
 
-          <li>
-            <div className="search">
-              <input type='text' placeholder="Search notes"/>
-              <i class="fas fa-search"></i>
-            </div>
-          </li>
-
-          {/* <li>
-            <span className="add">
-              <i class="fas fa-plus-circle"></i>
-            </span>
-          </li> */}
-        </ul>
-      </div>
-    // </div>
+        <li>
+          <div className="search">
+            <input type="text" placeholder="Search notes" />
+            <i class="fas fa-search"></i>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
 
