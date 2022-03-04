3cef8571b275c0d8890c39dac6382060c6176516
Abhay5855 <ap3919529@gmail.com>
2022-03-04 21:28:19 +0530

    docs : using the useCallback hook and debounce from lodash we are rendering the text from the editor after 1sec and to remove the html tag from the text rendered using dangerouslysetInnerHTML property.

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import "./editor.css";
 import ReactQuill from "react-quill";
 import Editor_Format, { modules, formats } from "../../utils/Editor_Format";
+import {debounce} from 'lodash';
 
 
 const Editor = () => {
@@ -11,20 +12,31 @@ const Editor = () => {
   //state to add text inside of editor
   const [text , setText] = useState('');
 
+   const deb = useCallback(
 
+    debounce((val) => setText(val) ,1000),
 
+    []
+
+   )
+  
   const handleChange = (val) => {
 
-       
-          setText(val);
+     
+     const txt =   deb(val);
+     console.log(txt);
 
-          console.log(text);
+           
+
+          
   }
 
     
   return (
     <div className="editor__container">
       <div className="editor">
+
+      <div dangerouslySetInnerHTML={{__html: text}} />
         <Editor_Format />
         <ReactQuill modules={modules} formats={formats} value={text} onChange={handleChange}/>
       </div>
