d125288157c5d4d1112e15fc2c5380cb9175504e
Abhay5855 <ap3919529@gmail.com>
2022-03-05 16:24:06 +0530

    refactor : creating a filter function directly and then map in the display variable.

--- src/notes/Notes.js
+++ src/notes/Notes.js
@@ -5,7 +5,29 @@ const Notes = ({ notes, filter, setFilter }) => {
   const [search, setSearch] = useState("");
 
  
-  const displayNotes = filter.map((note, idx) => {
+  // const displayNotes = filter.map((note, idx) => {
+  //   return (
+  //     <div className="notes">
+  //       <div key={idx} className="notes__container">
+  //         <p className="note__title">{note.data.title}</p>
+
+  //         <span className="note__date">
+  //           {/* {new Date(note.data.timestamp.toDate()).toDateString()} */}
+  //         </span>
+  //       </div>
+  //     </div>
+  //   );
+  // });
+
+  const displayNotes = notes.filter((item) => {
+
+    let searchStr = search.toLowerCase();
+      let nameMatches = item.data.title.toLowerCase().includes(searchStr);
+
+      return nameMatches;
+
+         
+  }).map((note, idx) => {
     return (
       <div className="notes">
         <div key={idx} className="notes__container">
@@ -19,21 +41,24 @@ const Notes = ({ notes, filter, setFilter }) => {
     );
   });
 
+
   const handleSearch = (e) => {
-    let value = e.target.value.toLowerCase();
+    // let value = e.target.value.toLowerCase();
 
-    setSearch(value);
+    // setSearch(value);
 
-    let res = [];
+    // let res = [];
 
-    res = notes.filter((data) => {
-      let searchStr = search.toLowerCase();
-      let nameMatches = data.data.title.toLowerCase().includes(searchStr);
+    // res = notes.filter((data) => {
+    //   let searchStr = search.toLowerCase();
+    //   let nameMatches = data.data.title.toLowerCase().includes(searchStr);
 
-      return nameMatches;
-    });
+    //   return nameMatches;
+    // });
 
-    setFilter(res);
+    // setFilter(res);
+
+    setSearch(e.target.value);
   };
 
   return (
