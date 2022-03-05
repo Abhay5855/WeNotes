15678fdf4dabb51e66f8c6ed430011ade1358346
Abhay5855 <ap3919529@gmail.com>
2022-03-06 00:49:46 +0530

    docs : adding the state for text , title , isUpdate and id and passing them as props to the Notes and Editor component.

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -6,12 +6,13 @@ import { debounce } from "lodash";
 import { db } from "../../firebase/firebase";
 import { query, onSnapshot } from "firebase/firestore";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 
-const Editor = () => {
+const Editor = ({ text, setText, isUpdate, id, title, setTitle }) => {
   //state to add text inside of editor
-  const [text, setText] = useState("");
-  const [id, setId] = useState("");
-  const [title, setTitle] = useState("");
+  // const [text, setText] = useState("");
+  // const [id, setId] = useState("");
+  // const [title, setTitle] = useState("");
   const [addingNote, setAddingNote] = useState(false);
 
   //using the useCallback hook to pass the debounce val and time delay of 1sec.
@@ -29,7 +30,6 @@ const Editor = () => {
   // handle submit button
   const handleAdd = async () => {
     setAddingNote(!addingNote);
-   
 
     await addDoc(collection(db, "note"), {
       title: title,
@@ -41,16 +41,31 @@ const Editor = () => {
       })
 
       .catch((err) => {
-        console.log(err); 
+        console.log(err);
       });
 
-      // Empty the results
-      setText("");
-      setTitle("");
+    // Empty the results
+    setText("");
+    setTitle("");
   };
 
   // Disable condition if text is empty
-  const disabled = !title || !text;
+  const disabled = !text;
+
+  const handleSubmit = async () => {
+    const updateRef = doc(db, "note", id);
+    try {
+      await updateDoc(updateRef, {
+        title: title,
+        text: text,
+      });
+    } catch (err) {
+      alert(err);
+    }
+
+    setText("");
+    setTitle("");
+  };
 
   return (
     <div className="editor__container">
@@ -58,13 +73,24 @@ const Editor = () => {
         {/* Using dangerousHtml property to remove the html tags and display plain text */}
         {/* <h2 dangerouslySetInnerHTML={{ __html: text }} /> */}
 
-        <button
-          disabled={disabled}
-          className="submit__note"
-          onClick={() => handleAdd()}
-        >
-          Submit Note
-        </button>
+        {isUpdate ? (
+          <button
+            // disabled={disabled}
+            className="submit__note"
+            onClick={() => handleSubmit()}
+          >
+            Update Note
+          </button>
+        ) : (
+          <button
+            disabled={disabled}
+            className="submit__note"
+            onClick={() => handleAdd()}
+          >
+            Submit Note
+          </button>
+        )}
+
         <input
           type="text"
           placeholder="Enter your note title.."
