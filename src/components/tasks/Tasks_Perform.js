af10a2a8e0de2d865b88315ffda7141b758ecd21
Abhay5855 <ap3919529@gmail.com>
2022-03-07 18:44:40 +0530

    fix :remove the checked state

--- src/components/tasks/Tasks_Perform.js
+++ src/components/tasks/Tasks_Perform.js
@@ -1,16 +1,16 @@
 
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import'./tasks.css'
 import { db } from '../../firebase/firebase'
-import { deleteDoc, doc, addDoc , collection} from "firebase/firestore";
+import { deleteDoc, doc} from "firebase/firestore";
 
 const Tasks_Perform = ({todos}) => {
 
+  const [isChecked, setIsChecked] = useState(false);
 
-
-
+   
   
 
   // Display all the dos using map method
@@ -20,7 +20,7 @@ const Tasks_Perform = ({todos}) => {
     return (
       <div key={todo.id} className='all__todos'>
 
-        <input type='checkbox' className='strikethrough' onClick={() => handleClicked(todo.id , todo.data.todo )}/>
+        <input type='checkbox' className='strikethrough'/>
         <p className='todo__name'>{todo.data.todo}</p>
         <span className='delete__todo' onClick={() => handleRemove(todo.id)}><i class="fas fa-times"></i></span>
         </div>
@@ -39,29 +39,7 @@ const Tasks_Perform = ({todos}) => {
     }
   };
 
-  const handleClicked = async(id , todo) => {
-
-    await addDoc(collection(db, "completed"), {
-      id : id,
-      completed : todo,
-    })
-      .then((res) => {
-        console.log(res);
-        const movieDocRef = doc(db, "todos", id);
-        try {
-          deleteDoc(movieDocRef);
-        } catch (err) {
-          alert(err);
-        }
-      })
-
-      .catch((err) => {
-        console.log(err);
-      });
-        
-         
-  }
-    
+  
 
   return (
     <div className='tasks__perform'> 
