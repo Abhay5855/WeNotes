570a04ea75badc16051debc60e2aacd7740fd41c
Abhay5855 <ap3919529@gmail.com>
2022-03-07 16:24:40 +0530

    docs : calling the useEffect and get all added todos and set the state with the timestamp.

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState , useEffect} from "react";
 import "./tasks.css";
 import TasksAdd from "./TasksAdd";
 import Tasks_Completed from "./Tasks_Completed";
 import Tasks_Perform from "./Tasks_Perform";
+import { db } from "../../firebase/firebase";
+import { query, onSnapshot , collection, orderBy} from "firebase/firestore";
 
 
 const Tasks = () => {
+
+
+  const [todos , setTodos] = useState([]);
+
+
+  // Get All the tasks 
+  useEffect(() => {
+    const GetAllTodos = () => {
+      
+      const allTodos = query(collection(db, "todos"), orderBy('timestamp'))
+
+      onSnapshot(allTodos, (QuerySnapshot) => {
+       
+        setTodos(
+          QuerySnapshot.docs.map((doc) => ({
+            id : doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+    };
+
+    GetAllTodos();
+  }, []);
+
+      
+
+
   return (
     <div className="task__container">
       <div className="task__section">
@@ -19,7 +49,7 @@ const Tasks = () => {
             {/* Added tasks */}
             <div className="performance">
               
-            <Tasks_Perform/>
+            <Tasks_Perform todos={todos}/>
               </div>
 
 
