98c6c839b0786ff3fe8bf8f7843bb00b6b42009b
Abhay5855 <ap3919529@gmail.com>
2022-03-07 16:23:18 +0530

    docs : add a cross icon tofor delete and map te todos.

--- src/components/tasks/Tasks_Perform.js
+++ src/components/tasks/Tasks_Perform.js
@@ -4,9 +4,35 @@
 import React from 'react'
 import'./tasks.css'
 
-const Tasks_Perform = () => {
+const Tasks_Perform = ({todos}) => {
+
+
+  console.log(todos)
+
+
+  const displayTodos = todos.map((todo)=> {
+
+
+    return (
+      <div key={todo.id} className='all__todos'>
+
+        <input type='checkbox' />
+        <p>{todo.data.todo}</p>
+        <span className='delete__todo'><i class="fas fa-times"></i></span>
+        
+        
+        </div>
+    )
+
+  })
+
+    
+
   return (
-    <div className='tasks__perform'>
+    <div className='tasks__perform'> 
+
+           
+        {displayTodos}
 
 
         
