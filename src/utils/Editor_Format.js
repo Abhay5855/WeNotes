5fc0ae9131cbe787d825b703e8a46646df9595cb
Abhay5855 <ap3919529@gmail.com>
2022-03-09 15:57:15 +0530

    docs: remov the custom buttons

--- src/utils/Editor_Format.js
+++ src/utils/Editor_Format.js
@@ -7,26 +7,26 @@ import '../components/editor/editor.css'
 // Custom Undo button icon component for Quill editor. You can import it directly
 // from 'quill/assets/icons/undo.svg' but I found that a number of loaders do not
 // handle them correctly
-const CustomUndo = () => (
-  <svg viewBox="0 0 18 18">
-    <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
-    <path
-      className="ql-stroke"
-      d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"
-    />
-  </svg>
-);
+// const CustomUndo = () => (
+//   <svg viewBox="0 0 18 18">
+//     <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
+//     <path
+//       className="ql-stroke"
+//       d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"
+//     />
+//   </svg>
+// );
 
 // Redo button icon component for Quill editor
-const CustomRedo = () => (
-  <svg viewBox="0 0 18 18">
-    <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
-    <path
-      className="ql-stroke"
-      d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"
-    />
-  </svg>
-);
+// const CustomRedo = () => (
+//   <svg viewBox="0 0 18 18">
+//     <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
+//     <path
+//       className="ql-stroke"
+//       d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"
+//     />
+//   </svg>
+// );
 
 // Undo and redo functions for Custom Toolbar
 function undoChange() {
